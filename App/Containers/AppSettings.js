d42dce0aa5332194a03c676a4bcea34ae5b512ba
Sibasish Aich <sibasisha671@IN.nam.ad.pwcinternal.com>
2018-09-06 18:30:44 +0530

    Login Screen and IP config

--- App/Containers/AppSettings.js
+++ App/Containers/AppSettings.js
@@ -1,92 +1,115 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image,AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, Image, AsyncStorage } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { connect } from "react-redux";
 import styles from './Styles/LaunchScreenStyles';
 import { Images } from '../Themes';
-import {Button,Content } from 'native-base';
+import { Button, Content } from 'native-base';
 import ReduxActions from "../Redux/ActionTypes/Action";
-import {NavigationActions } from 'react-navigation';
+import { NavigationActions } from 'react-navigation';
 import TextBoxMaterial from "../Components/TextBox";
-import {Toast} from 'native-base';
+import { Toast } from 'native-base';
+import { isNullOrUndefined } from 'util';
 
 class AppSettingsComponent extends React.Component {
     constructor() {
         super();
     }
 
-    componentWillMount(){
-       this.getUserId();
+    componentWillMount() {
+        this.GetIP_Port();
     }
-    async getUserId () {
+
+
+    async GetIP_Port() {
         try {
-          const IPAddress = await AsyncStorage.getItem('IP');
-          const Port = await AsyncStorage.getItem('Port');
-          this.props.dispatch({type:ReduxActions.SET_IP_ADDRESS,IP:IPAddress});
-          this.props.dispatch({type:ReduxActions.SET_PORT,Port:Port});
-          return true;
+            const IPAddress = await AsyncStorage.getItem('IP');
+            const Port = await AsyncStorage.getItem('Port');
+            this.props.dispatch({ type: ReduxActions.SET_IP_ADDRESS, IP: isNullOrUndefined(IPAddress) ? "" : IPAddress });
+            this.props.dispatch({ type: ReduxActions.SET_PORT, PortAddress: isNullOrUndefined(Port) ? "" : Port });
+            return true;
         } catch (error) {
-          // Error retrieving data
-          console.log(error.message);
-          return false;
+            // Error retrieving data
+            console.log(error.message);
+            return false;
         }
     }
 
-    changeField(changedText,type) {
-        if(type==="ip"){
-        if (/^(?=.*[^\.]$)((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){4}$/.test(changedText))
-        {
-         this.props.dispatch({type:ReduxActions.SET_IP_ADDRESS,IP:changedText});
+    changeField(changedText, type) {
+        if (type === "ip") {
+            debugger;
+            this.props.dispatch({ type: ReduxActions.SET_IP_ADDRESS, IP: isNullOrUndefined(changedText) ? "" : changedText });
+            //if (/^(?=.*[^\.]$)((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){4}$/.test(changedText)) {
+            //     this.props.dispatch({ type: ReduxActions.SET_IP_ADDRESS, IP: changedText });
+            // }
+            // if (/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(changedText)) {
+            //     
+            //     this.props.dispatch({ type: ReduxActions.SET_IP_ADDRESS, IP: changedText });
+            // }
         }
-       }
-     else if(type==="port"){
-        this.props.dispatch({type:ReduxActions.SET_PORT,Port:changedText});
-     }
+        else if (type === "port") {
+            this.props.dispatch({ type: ReduxActions.SET_PORT, PortAddress: isNullOrUndefined(changedText) ? "" : changedText });
+        }
     }
 
-    async saveAppSettings(){
-       await AsyncStorage.setItem('IP', this.props.ipAddress);
-       await AsyncStorage.setItem('Port', this.props.port);
-       Toast.show({
-        text: 'IP Address & Port saved successfully',
-        textStyle: { fontSize: 25, fontFamily:'Avenir-Black',fontWeight:'bold' },
-        duration: 2000,
-        buttonTextStyle:{fontSize: 20, fontFamily:'Avenir-Black'},
-        buttonText: "Okay",
-        type: "success"
-        });
-        const resetAction = NavigationActions.reset({
-            index: 0,
-            key: null,
-            actions: [
-                NavigationActions.navigate({routeName: 'CaptainDashboardScreen'})
-            ]
-        });
-        this.props.navigation.dispatch(resetAction);
+    async saveAppSettings() {
+        debugger;
+        let regex=/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+        if (regex.test(this.props.ipAddress)) {
+            await AsyncStorage.setItem('IP', this.props.ipAddress);
+            await AsyncStorage.setItem('Port', this.props.port);
+            Toast.show({
+                text: 'IP Address & Port saved successfully',
+                textStyle: { fontSize: 25, fontFamily: 'Avenir-Black', fontWeight: 'bold' },
+                duration: 2000,
+                buttonTextStyle: { fontSize: 20, fontFamily: 'Avenir-Black' },
+                buttonText: "Okay",
+                type: "success"
+            });
+
+            const resetAction = NavigationActions.reset({
+                index: 0,
+                key: null,
+                actions: [
+                    NavigationActions.navigate({ routeName: 'CaptainDashboardScreen' })
+                ]
+            });
+            this.props.navigation.dispatch(resetAction);
+        }
+        else {
+            Toast.show({
+                text: 'Please provide valid ip address!',
+                textStyle: { fontSize: 25, fontFamily: 'Avenir-Black', fontWeight: 'bold' },
+                duration: 2000,
+                buttonTextStyle: { fontSize: 20, fontFamily: 'Avenir-Black' },
+                buttonText: "Okay",
+                type: "danger"
+            });
+        }
     }
 
     render() {
-        console.log(this.props);
+
         return (
             <View style={styles.mainContainer}>
-            <Image source={Images.background} style={styles.backgroundImage} resizeMode='cover' />
-            <Content style={{marginLeft:2+'%'}}>
-                <TextBoxMaterial
-                            label="IP Address"
-                            value={this.props.ipAddress}
-                            changeField={this.changeField.bind(this)}
-                            type="ip"/>
+                <Image source={Images.background} style={styles.backgroundImage} resizeMode='cover' />
+                <Content style={{ marginLeft: 2 + '%' }}>
+                    <TextBoxMaterial
+                        label="IP Address"
+                        value={this.props.ipAddress}
+                        changeField={this.changeField.bind(this)}
+                        type="ip" />
                     <TextBoxMaterial
                         label="Port"
                         value={this.props.port}
                         changeField={this.changeField.bind(this)}
-                        type="port"/>        
-             </Content>
-                <View style={{flex:2,flexDirection:'row',marginRight:2+'%',alignItems:'flex-end',justifyContent:'flex-end'}}>
-                 <Button style={{height:50,width:200,justifyContent:'center'}}  onPress={this.saveAppSettings.bind(this)}>
-                        <Icon active name="content-save-all" size={24} color="#FAFAFA"/>
-                        <Text style={stylesDrawer.textStyle}>Save</Text>                        
-                        </Button>
+                        type="port" />
+                </Content>
+                <View style={{ flex: 2, flexDirection: 'row', marginRight: 2 + '%', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+                    <Button style={{ height: 50, width: 200, justifyContent: 'center' }} onPress={this.saveAppSettings.bind(this)}>
+                        <Icon active name="content-save-all" size={24} color="#FAFAFA" />
+                        <Text style={stylesDrawer.textStyle}>Save</Text>
+                    </Button>
                 </View>
             </View>
         )
@@ -94,20 +117,21 @@ class AppSettingsComponent extends React.Component {
 }
 
 const stylesDrawer = StyleSheet.create({
-    textStyle:{
-      fontSize:30,
-      fontWeight: 'bold',
-      fontFamily:'Avenir-Black',
-      color:'white'
+    textStyle: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontFamily: 'Avenir-Black',
+        color: 'white'
     }
 })
 
-    
-const mapStateToProps=(state)=>{
+
+const mapStateToProps = (state) => {
+
     return {
-        ipAddress : state.DashBoardReducer.ipAddress,
-        port: state.DashBoardReducer.port
+        ipAddress: state.DashBoardReducer.setipAddress,
+        port: state.DashBoardReducer.setPort
     }
-  }
-  
-  export default connect(mapStateToProps,null)(AppSettingsComponent)
+}
+
+export default connect(mapStateToProps, null)(AppSettingsComponent)
--- App/Containers/CaptainDashboard.js
+++ App/Containers/CaptainDashboard.js
@@ -1,119 +1,169 @@
 import React, { Component } from "react";
-import { Card, CardItem,Right } from "native-base";
-import { Text, Image, View, StyleSheet,Alert} from 'react-native'
+import { Card, CardItem, Right } from "native-base";
+import { Text, Image, View, StyleSheet, Alert, AsyncStorage } from 'react-native'
 import styles from './Styles/LaunchScreenStyles';
 import { Images } from '../Themes';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { connect } from 'react-redux';
 import ReduxActions from "../Redux/ActionTypes/Action";
+import { isNullOrUndefined } from "util";
+import { Toast } from 'native-base';
 
 class CaptainDashboardComponent extends Component {
 
-// componentWillMount(){
-//   getIP();
-//   getPort();
-// }
-// async getIP () {
-//         try {
-//           const IPAddress = await AsyncStorage.getItem('IP');
-//           return IPAddress;
-//         }
-//         catch(ex){}
-//       }
-//       async getPort () {
-//         try {
-//       const Port = await AsyncStorage.getItem('Port');
-//       return Port;
-//     }
-//     catch(ex){}
-//   }
+  // componentWillMount() {
+  //   debugger;
+  //this.GetIP_Port();
+  // AsyncStorage.getItem('IP').then((value) => {
+  //   if (isNullOrUndefined(value) && value !== "") {
+  //     debugger;
+  //     this.props.dispatch({ type: ReduxActions.SET_IP_ADDRESS, IP: isNullOrUndefined(value) ? "" : value });
+  //   }
+  // });
+  // AsyncStorage.getItem('Port').then((value) => {
+  //   if (isNullOrUndefined(value) && value !== "") {
+  //     debugger;
+  //     this.props.dispatch({ type: ReduxActions.SET_PORT, PortAddress: isNullOrUndefined(value) ? "" : value });
+  //   }
+  // });
+  //}
+
+  async GetIP_Port() {
+    try {
+      debugger;
+      const IPAddress = await AsyncStorage.getItem('IP');
+      const Port = await AsyncStorage.getItem('Port');
+      this.props.dispatch({ type: ReduxActions.GET_IP_ADDRESS, IP: isNullOrUndefined(IPAddress) ? "" : IPAddress });
+      this.props.dispatch({ type: ReduxActions.GET_PORT, PortAddress: isNullOrUndefined(Port) ? "" : Port });
+      return true;
+    } catch (error) {
+      // Error retrieving data
+      console.log(error.message);
+      return false;
+    }
+  }
+
+  ValidateAPIEndpoint(event, val) {
+    if (isNullOrUndefined(this.props.ipAddress) || isNullOrUndefined(this.props.port)
+      || this.props.ipAddress === "" || this.props.port === "") {
+      Toast.show({
+        text: "Please provide ip details before proceeding!",
+        textStyle: { fontSize: 25, fontFamily: 'Avenir-Black' },
+        duration: 2000,
+        position: "bottom",
+        buttonTextStyle: { fontSize: 20, fontFamily: 'Avenir-Black' },
+        buttonText: "Ok",
+        type: "danger"
+      });
+    } else {
+      if (val === "NewUser") {
+        this.props.navigation.navigate('BeforeModeSelectionStack');
+      }
+      else if (val === "ExistingUser") {
+        this.props.navigation.navigate('ExistingOrderStack')
+      }
+      else if (val === "ModifyUser") {
+        this.props.navigation.navigate('CaptainStack')
+      }
+    }
+  }
 
 
-render() {
+  render() {
+    debugger;
+    this.GetIP_Port();
+    console.log(this.props.ipAddress, this.props.port);
+    let CheckIP = '';
+    debugger;
+    if (this.props.ipAddress === null || this.props.ipAddress === "" || this.props.port === null || this.props.port === "") {
+      CheckIP = 'IP Configuration';
+    } else {
+      CheckIP = 'Change IP';
+    }
     return (
-        <View style={styles.mainContainer}>
-       <View style={stylesDrawer.viewStyle}>
-            <Text style={stylesDrawer.textStyle}>Dashboard</Text>
+      <View style={styles.mainContainer}>
+        <View style={stylesDrawer.viewStyle}>
+          <Text style={stylesDrawer.textStyle}>Dashboard</Text>
         </View>
         <Image source={Images.background} style={styles.backgroundImage} resizeMode='cover' />
-      <Card style={{flex:10}}>
-      <CardItem header bordered>
-        <Text  style= {styles.sectionText}>Order</Text>
-      </CardItem>
-      <CardItem button bordered style={{cursor:'pointer'}} onPress={() =>this.props.navigation.navigate('BeforeModeSelectionStack')}>
-      <Icon active name="local-dining" size={42} style={{  color: "#fbc02d" }} />
-      <View style={{width: 80 + '%' }}><Text style={styles.subtitle}>
-            New Order
+        <Card style={{ flex: 10 }}>
+          <CardItem header bordered>
+            <Text style={styles.sectionText}>Order</Text>
+          </CardItem>
+          <CardItem button bordered style={{ cursor: 'pointer' }} onPress={(event) => this.ValidateAPIEndpoint(event, "NewUser")}>
+            <Icon active name="local-dining" size={42} style={{ color: "#fbc02d" }} />
+            <View style={{ width: 80 + '%' }}><Text style={styles.subtitle}>
+              New Order
           </Text></View>
-          <View style={{width: 10 + '%' }}>
-                <Icon active size={42} name="arrow-forward" />
-          </View>
-      </CardItem>
-      {/* <View style={{ borderWidth: 0.5, borderColor: 'black', margin: 10 }} /> */}
-      <CardItem button bordered style={{cursor:'pointer'}} onPress={() => this.props.navigation.navigate('ExistingOrderStack')}>
-      <Icon active name="local-grocery-store" size={42} style={{  color: "#ff9800" }} />
-          <View style={{width: 80 + '%' }}><Text style={styles.subtitle}>
-            Existing Order
+            <View style={{ width: 10 + '%' }}>
+              <Icon active size={42} name="arrow-forward" />
+            </View>
+          </CardItem>
+          {/* <View style={{ borderWidth: 0.5, borderColor: 'black', margin: 10 }} /> */}
+          <CardItem button bordered style={{ cursor: 'pointer' }} onPress={(event) => this.ValidateAPIEndpoint(event, "ExistingUser")}>
+            <Icon active name="local-grocery-store" size={42} style={{ color: "#ff9800" }} />
+            <View style={{ width: 80 + '%' }}><Text style={styles.subtitle}>
+              Existing Order
           </Text></View>
-          <View style={{width: 10 + '%' }}>
-                <Icon active size={42} name="arrow-forward" />
-          </View>
-      </CardItem>
-      {/* <View style={{ borderWidth: 1 , borderColor: 'black', margin: 10 }} /> */}
-      <CardItem header bordered>
-        <Text style= {styles.sectionText}>Customer</Text>
-      </CardItem>
-      <CardItem button bordered style={{cursor:'pointer'}} onPress={() => this.props.navigation.navigate('CaptainStack')}>
-      <Icon active name="person" size={42} style={{  color: "#039be5" }} />
-      <View style={{width: 80 + '%' }}><Text style={styles.subtitle}>
-            Modify
+            <View style={{ width: 10 + '%' }}>
+              <Icon active size={42} name="arrow-forward" />
+            </View>
+          </CardItem>
+          {/* <View style={{ borderWidth: 1 , borderColor: 'black', margin: 10 }} /> */}
+          <CardItem header bordered>
+            <Text style={styles.sectionText}>Customer</Text>
+          </CardItem>
+          <CardItem button bordered style={{ cursor: 'pointer' }} onPress={(event) => this.ValidateAPIEndpoint(event, "ModifyUser")}>
+            <Icon active name="person" size={42} style={{ color: "#039be5" }} />
+            <View style={{ width: 80 + '%' }}><Text style={styles.subtitle}>
+              Modify
           </Text></View>
-          <View style={{width: 10 + '%' }}>
-                <Icon active size={42} name="arrow-forward" />
-          </View>
-      </CardItem>
-      <CardItem header bordered>
-        <Text style= {styles.sectionText}>Settings</Text>
-      </CardItem>
-      <CardItem button bordered style={{cursor:'pointer'}} onPress={() => this.props.navigation.navigate('AppSettingStack')}>
-      <Icon active name="settings" size={42} style={{  color: "#039be5" }} />
-      <View style={{width: 80 + '%' }}><Text style={styles.subtitle}>
-            Set Api Endpoint
-          </Text></View>
-          <View style={{width: 10 + '%' }}>
-                <Icon active size={42} name="arrow-forward" />
-                </View>
-      </CardItem>
-    </Card>
-    </View>
+            <View style={{ width: 10 + '%' }}>
+              <Icon active size={42} name="arrow-forward" />
+            </View>
+          </CardItem>
+          <CardItem header bordered>
+            <Text style={styles.sectionText}>{CheckIP}</Text>
+          </CardItem>
+          <CardItem button bordered style={{ cursor: 'pointer' }} onPress={() => this.props.navigation.navigate('LoginStack')}>
+            <Icon active name="settings" size={42} style={{ color: "#039be5" }} />
+            <View style={{ width: 80 + '%' }}><Text style={styles.subtitle}>
+              Log In
+                </Text></View>
+            <View style={{ width: 10 + '%' }}>
+              <Icon active size={42} name="arrow-forward" />
+            </View>
+          </CardItem>
+        </Card>
+      </View>
 
     );
   }
 }
 
 const stylesDrawer = StyleSheet.create({
-viewStyle:{
-  flex:1,
-  flexDirection:'row',
-  backgroundColor: '#039be5',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height:60,
-  paddingTop:15,
-  elevation:2
-},
-textStyle:{
-  fontSize:30,
-  fontWeight: 'bold',
-  fontFamily:'Avenir-Black',
-  color:'white'
-}
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#039be5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    elevation: 2
+  },
+  textStyle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontFamily: 'Avenir-Black',
+    color: 'white'
+  }
 })
 
 const mapStateToProps = (state) => {
-  return{
-    ipAddress : state.DashBoardReducer.ipAddress,
-    port: state.DashBoardReducer.port
+  return {
+    ipAddress: state.DashBoardReducer.getipAddress,
+    port: state.DashBoardReducer.getport
   };
 }
 export default connect(mapStateToProps, null)(CaptainDashboardComponent)
